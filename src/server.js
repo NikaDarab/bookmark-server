fec4446a6a87e6baac20ec6a8669410273dec329
Nika Darab <nikadarab@pm.me>
2021-01-11 16:35:52 -0600

    knex, error handler

--- src/server.js
+++ src/server.js
@@ -1,8 +1,13 @@
 /* eslint-disable no-console */
 /* eslint-disable quotes */
+const knex = require("knex");
 const app = require("./app");
-const { PORT } = require("./config");
+const { PORT, DB_URL } = require("./config");
 
+const db = knex({
+  client: "pg",
+  connection: "DB_URl",
+});
 app.listen(PORT, () => {
   console.log(`Server listening at http://localhost:${PORT}`);
 });
